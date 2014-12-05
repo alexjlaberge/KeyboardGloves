056ec0a929019dca629cf60645373d28ebccb66f
Alex LaBerge <alexjlaberge@gmail.com>
2014-12-04 22:35:46 -0500

    Added serial and mouse movement to main loop

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,26 @@
 #include "findEyeCenter.h"
 #include "findEyeCorner.h"
 
+#include <unistd.h>
 
-/** Constants **/
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
 
+#include <vector>
+
+#include <stdio.h>      // standard input / output functions
+#include <stdlib.h>
+#include <string.h>     // string function definitions
+#include <unistd.h>     // UNIX standard function definitions
+#include <fcntl.h>      // File control definitions
+#include <errno.h>      // Error number definitions
+#include <termios.h>    // POSIX terminal control definitions
+#include <iostream>
 
 /** Function Headers */
-void detectAndDisplay( cv::Mat frame );
+std::vector<int> detectAndDisplay( cv::Mat frame );
 
 /** Global variables */
-//-- Note, either copy these two files from opencv/data/haarscascades to your current folder, or change these locations
 cv::String face_cascade_name = "../res/haarcascade_frontalface_alt.xml";
 cv::CascadeClassifier face_cascade;
 std::string main_window_name = "Capture - Face detection";
@@ -30,61 +41,215 @@ cv::Mat skinCrCbHist = cv::Mat::zeros(cv::Size(256, 256), CV_8UC1);
 
 using namespace std;
 
-/**
- * @function main
- */
 int main( int argc, const char** argv ) {
   CvCapture* capture;
   cv::Mat frame;
 
+  Display *display = XOpenDisplay(0);
+  Window root = DefaultRootWindow(display);
+    //for(int x = 0; x < 1000; x++){for(int y = 0; y < 1000; y++){XWarpPointer(display, None, root, 0, 0, 0, 0, x, y); usleep(100);}}
+  XFlush(display);
+
+
+  XCloseDisplay(display);
+
+  int USB = open( "/dev/ttyUSB0", O_RDWR| O_NOCTTY );
+  struct termios tty;
+  struct termios tty_old;
+  memset (&tty, 0, sizeof tty);
+
+  /* Error Handling */
+  if ( tcgetattr ( USB, &tty ) != 0 )
+  {
+      cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << endl;
+  }
+
+  /* Save old tty parameters */
+  tty_old = tty;
+
+  /* Set Baud Rate */
+  cfsetospeed (&tty, (speed_t)B9600);
+  cfsetispeed (&tty, (speed_t)B9600);
+
+  /* Setting other Port Stuff */
+  tty.c_cflag     &=  ~PARENB;        // Make 8n1
+  tty.c_cflag     &=  ~CSTOPB;
+  tty.c_cflag     &=  ~CSIZE;
+  tty.c_cflag     |=  CS8;
+
+  tty.c_cflag     &=  ~CRTSCTS;       // no flow control
+  tty.c_cc[VMIN]      =   1;                  // read doesn't block
+  tty.c_cc[VTIME]     =   5;                  // 0.5 seconds read timeout
+  tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+  cout << "test" << flush;
+  /* Make raw */
+  cfmakeraw(&tty);
+
+  /* Flush Port, then applies attributes */
+  tcflush( USB, TCIFLUSH );
+  if ( tcsetattr ( USB, TCSANOW, &tty ) != 0)
+  {
+      cout << "Error " << errno << " from tcsetattr" << endl;
+  }
+
+    //XWarpPointer(display, None, root, 0, 0, 0, 0, x, y);
   // Load the cascades
   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade, please change face_cascade_name in source code.\n"); return -1; };
 
-  cv::namedWindow(main_window_name,CV_WINDOW_NORMAL);
-  cv::moveWindow(main_window_name, 400, 100);
-  cv::namedWindow(face_window_name,CV_WINDOW_NORMAL);
-  cv::moveWindow(face_window_name, 10, 100);
-  cv::namedWindow("Right Eye",CV_WINDOW_NORMAL);
-  cv::moveWindow("Right Eye", 10, 600);
-  cv::namedWindow("Left Eye",CV_WINDOW_NORMAL);
-  cv::moveWindow("Left Eye", 10, 800);
-  cv::namedWindow("aa",CV_WINDOW_NORMAL);
-  cv::moveWindow("aa", 10, 800);
-  cv::namedWindow("aaa",CV_WINDOW_NORMAL);
-  cv::moveWindow("aaa", 10, 800);
+
 
   createCornerKernels();
   ellipse(skinCrCbHist, cv::Point(113, 155.6), cv::Size(23.4, 15.2),
           43.0, 0.0, 360.0, cv::Scalar(255, 255, 255), -1);
 
-   // Read the video stream
+  // Read the video stream
   capture = cvCaptureFromCAM( -1 );
-  //  CSerial Talker;
-    //Talker.Open(3);
-    //Talker.SendData("a", 1);
-  if( capture ) {
-    while( true ) {
+  
+  int i = 1;
+  bool check = true;
+  int x = 960, y = 540;
+  
+  if( capture ) 
+  {
+    while( check ) 
+    {
       frame = cvQueryFrame( capture );
       // mirror it
       cv::flip(frame, frame, 1);
       frame.copyTo(debugImage);
-
-      // Apply the classifier to the frame
+      // Apply the class>ier to the frame
+      vector<int> result;
       if( !frame.empty() ) {
-        detectAndDisplay( frame );
+        result = detectAndDisplay( frame );
       }
-      else {
+      else 
+      {
         printf(" --(!) No captured frame -- Break!");
         break;
       }
-
-      imshow(main_window_name,debugImage);
-
+      //120 ... 130 ... 140
+      //0 ... 960 ... 1920 for x
+      //45 ... 60 ... 75
+      //0 ... 540 ... 1080
+      int dX = result[0] - 130;
+      int dY = result[1] - 60;
+      dX = dX * (1920 / 20);
+      x = x + dX; 
+      dY = dY * (1080/30);
+      y = y + dY;
       int c = cv::waitKey(10);
       if( (char)c == 'c' ) { break; }
-      if( (char)c == 'f' ) {
-        imwrite("frame.png",frame);
+      if( (char)c == 'f' ) { }
+      i++;
+      usleep(10000);
+      //READ
+      int n = 0;
+      char buf = '\0';
+
+      /* Whole response*/
+      std::string response;
+
+      do
+      {
+         n = read( USB, &buf, 1 );
+         response.append( &buf );
+      }while(buf != '\r' && n > 0);
+
+      if (n < 0)
+      {
+         cout << "Error reading: " << strerror(errno) << endl;
       }
+      else if (n == 0)
+      {
+          cout << "Read nothing!" << endl;
+      }
+      else
+      {
+          cout << "Response: " << response;
+          response = response.substr(1,1);
+          switch(*(response.c_str()))
+          {
+            case 'a':
+              cout << 'a';
+              break;
+            case 'b':
+              cout << 'b';
+              break;
+            case 'c':
+              cout << 'c';
+              break;
+            case 'd':
+              cout << 'd';
+              break;
+            case 'e':
+              cout << 'e';
+              break;
+            case 'f':
+              cout << 'f';
+              break;
+            case 'g':
+              cout << 'g';
+              break;
+            case 'h':
+              cout << 'h';
+              break;
+            case 'i':
+              cout << 'i';
+              break;
+            case 'j':
+              cout << 'j';
+              break;
+            case 'k':
+              cout << 'k';
+              break;
+            case 'l':
+              cout << 'l';
+              break;
+            case 'm':
+              cout << 'm';
+              break;
+            case 'n':
+              cout << 'n';
+              break;
+            case 'o':
+              cout << 'o';
+              break;
+            case 'p':
+              cout << 'p';
+              break;
+            case 'q':
+              cout << 'q';
+              break;
+            case 'r':
+              cout << 'r';
+              break;
+            case 's':
+              cout << 's';
+              break;
+            case 't':
+              cout << 't';
+              break;
+            case 'u':
+              cout << 'u';
+              break;
+            case 'v':
+              cout << 'v';
+              break;
+            case 'w':
+              cout << 'w';
+              break;
+            case 'x':
+              cout << 'x';
+              break;
+            case 'y':
+              cout << 'y';
+              break;
+            case 'z':
+              cout << 'z';
+              break;
+          }
+      }
+      //XWarpPointer(display, None, root, 0, 0, 0, 0, 960, 540);
 
     }
   }
@@ -94,7 +259,7 @@ int main( int argc, const char** argv ) {
   return 0;
 }
 
-void findEyes(cv::Mat frame_gray, cv::Rect face) {
+vector<int> findEyes(cv::Mat frame_gray, cv::Rect face) {
   cv::Mat faceROI = frame_gray(face);
   cv::Mat debugFace = faceROI;
 
@@ -143,7 +308,7 @@ void findEyes(cv::Mat frame_gray, cv::Rect face) {
   leftPupil.x += leftEyeRegion.x;
   leftPupil.y += leftEyeRegion.y;
   // draw eye centers
-	cout << "RIGHT PUPIL X,Y: " << rightPupil.x << " " << rightPupil.y << endl;
+	//cout << "RIGHT PUPIL X,Y: " << rightPupil.x << " " << rightPupil.y << endl;
   circle(debugFace, rightPupil, 3, 1234);
   circle(debugFace, leftPupil, 3, 1234);
 
@@ -167,7 +332,13 @@ void findEyes(cv::Mat frame_gray, cv::Rect face) {
     circle(faceROI, rightRightCorner, 3, 200);
   }
 
-  imshow(face_window_name, faceROI);
+  //imshow(face_window_name, faceROI);
+  vector<int> result;
+  result.push_back(leftPupil.x);
+  result.push_back(leftPupil.y);
+  result.push_back(rightPupil.x);
+  result.push_back(rightPupil.y);
+  return result;
 //  cv::Rect roi( cv::Point( 0, 0 ), faceROI.size());
 //  cv::Mat destinationROI = debugImage( roi );
 //  faceROI.copyTo( destinationROI );
@@ -198,7 +369,7 @@ cv::Mat findSkin (cv::Mat &frame) {
 /**
  * @function detectAndDisplay
  */
-void detectAndDisplay( cv::Mat frame ) {
+vector<int> detectAndDisplay( cv::Mat frame ) {
   std::vector<cv::Rect> faces;
   //cv::Mat frame_gray;
 
@@ -217,8 +388,10 @@ void detectAndDisplay( cv::Mat frame ) {
   {
     rectangle(debugImage, faces[i], 1234);
   }
+  vector<int> result;
   //-- Show what you got
   if (faces.size() > 0) {
-    findEyes(frame_gray, faces[0]);
+    result = findEyes(frame_gray, faces[0]);
   }
+  return result;
 }
